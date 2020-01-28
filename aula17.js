5a8b493275a69e9e9b0f3e789ed21cef2707fb3e
Gabriel Chagas <gabrielserq@outlook.com>
2020-01-28 11:39:37 -0300

    logando em um banco no mysql com o sequelize

---
+++ aula17.js
@@ -0,0 +1,11 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('test', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize.authenticate().then(function(){
+    console.log('Conectado com sucesso');
+}).catch(function(erro){
+    console.log(`Falha ao se conectar ${erro}`) ;
+});
