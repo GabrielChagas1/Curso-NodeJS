fd1783e0b18212acf7940d9b10731c59f7c914f4
Gabriel Chagas <gabrielserq@outlook.com>
2020-01-28 10:36:37 -0300

    terminando módulo sql básico

--- teste.sql
+++ teste.sql
@@ -1,5 +1,12 @@
 CREATE TABLE usuarios(
+    id INT AUTO_INCREMENT NOT NULL
     nome VARCHAR(50),
     email VARCHAR(100), 
     idade INT
+);
+
+INSERT INTO usuarios (nome, email, idade) VALUES(
+    "Rainer Resende",
+    "rainer@rainer.com",
+    19
 );
