db3bab8ff2788ef9097b0668752cdda82f1186f9
GabrielChagas1 <gabrielserq@outlook.com>
2020-02-05 01:52:53 -0200

    aprendendo a salvar uma categoria no mongoDB

--- Projeto Prático/routes/admin.js
+++ Projeto Prático/routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Categoria');
+const Categoria = mongoose.model('categorias');
 
 // rota inicial
 router.get('/', (req, res) =>{
@@ -19,6 +22,20 @@ router.get('/categorias', (req, res) =>{
 // rota para cadastrar uma categoria
 router.get('/categorias/add', (req, res) =>{
     res.render('admin/addCategoria');
+});
+
+// post para cadastrar um post novo
+router.post('/categoria/add', (req, res) =>{
+    // criando "objeto"
+    const newCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+    new Categoria(newCategoria).save().then(() =>{
+        console.log('Categoria salva com sucesso!');
+    }).catch((err) => {
+        console.log(`Erro ao salvar categoria: ${err}`);
+    });
 });
 
 
