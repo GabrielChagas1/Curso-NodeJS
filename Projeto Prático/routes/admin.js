b0f3434c977f9898e507269011491e2e2a5455bb
GabrielChagas1 <gabrielserq@outlook.com>
2020-02-05 01:28:30 -0200

    construindo páginas de categorias

--- Projeto Prático/routes/admin.js
+++ Projeto Prático/routes/admin.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+// rota inicial
 router.get('/', (req, res) =>{
     res.render('admin/index');
 });
 
+// rota para visualizar os posts
 router.get('/posts', (req, res) =>{
-    res.send('Página de Posts');
+    // res.render('admin/categorias');
 });
 
+// rota para visualizar as categorias
 router.get('/categorias', (req, res) =>{
-    res.send('Cadastrar categorias');
+    res.render('admin/categorias');
+});
+
+// rota para cadastrar uma categoria
+router.get('/categorias/add', (req, res) =>{
+    res.render('admin/addCategoria');
 });
 
 
