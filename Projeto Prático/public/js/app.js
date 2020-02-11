aa8a0f97ce50aed0394de812acbf2ea6a8613dba
Gabriel Chagas <gabrielserq@outlook.com>
2020-02-11 09:42:16 -0300

    alterando versão do jquery para funcionar o preview do dropify

--- Projeto Prático/public/js/app.js
+++ Projeto Prático/public/js/app.js
@@ -1,4 +1,12 @@
 $(document).ready(() =>{
     // iniciando o plugin dropify
-    $('.dropify').dropify();
+    // $('.dropify').dropify();
+    $('.dropify').dropify({
+        messages: {
+            'default': 'Arraste e solte um arquivo ou clique aqui',
+            'replace': 'Arraste e solte ou clique para substituir',
+            'remove':  'Remover',
+            'error':   'Ooops, something wrong happended.'
+        }
+    });
 });
